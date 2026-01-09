d88f8c12318636173a92a5d1a74bca95272e3d99
vijay kumar Yagavinti <yvijaykumar200210@gmail.com>
2026-01-09 19:53:45 +0530

    added simple.js file

---
+++ simple.js
@@ -0,0 +1,5 @@
+console.log("Hello, World!");
+console.log("This is a test commit");
+console.log("Adding more lines to test git functionality.");
+console.log("Final line for this simple git practice file.");
+console.log("End of the simple git practice file.");
